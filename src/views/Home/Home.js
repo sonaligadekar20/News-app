b36f43f6e2fd55e7fdc51d115fdbd15f50c26274
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-10 18:57:28 +0530

    news app ready

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,56 @@
-import React, { useState } from 'react'
+// import React from "react"
+import React, { useState, useEffect } from 'react'
 import axios from "axios"
 import "./Home.css"
+import NewsArticle from '../../components/NewsArticle/NewsArticle'
 
+function Home(){
+    const [news, setNews] = useState([])
+    const [searchQuery, setSearchQuery] = useState("pune")
+
+    const loadNews = async () => {
+        try{
+            const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-10-08&to=2023-10-08&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`);
+        setNews(response.data.articles)
+        }
+        catch(error){
+            console.log(error)
+        }
+        
+    }
+
+    useEffect(() => {
+        loadNews()
+    },[])
+
+    useEffect(() => {
+
+    }, [searchQuery])
+
+    return (
+        <div>
+            <h1>News App</h1>
+            
+            <input type="text" 
+            className='search-input'
+            value={searchQuery} 
+            onChange={(e)=>
+            setSearchQuery(e.target.value)}/>
+
+            <div className="news-container">
+            {
+                news.map((newsArticle, index)=>{
+                    const {author, title, description, url, urlToImage, publishedAt, content} = newsArticle
+                    return (
+                       <NewsArticle author={author} title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} content={content} key={index} />
+                       )    
+                })
+            }
+
+
+            </div>
+           
+        </div>
+    )   
+}
+export default Home 
